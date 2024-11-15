b85279f0a18c8490903a5f2b96e0bed712507655
mohammed kaif <mkaif555123@gmail.com>
2024-11-15 18:59:00 +0500

    array travesing practice with loop and fnction completed

--- Array Travesing/script.js
+++ Array Travesing/script.js
@@ -1,1 +1,12 @@
-console.log("Dsa With JS");
+// Array Travesing
+
+const data = [42, 56, 52, 2, 54, 3, 7, 67, 3, 10, 0, 97];
+
+// for (let i = 0; i < data.length; i++) {
+//     console.log(data[i]);
+// };
+
+function element() {
+    const input = document.getElementById("inp").value;
+    alert(data[input])
+};
