dc98b866bec605178d32739a59980c1d02be1018
mohammed kaif <mkaif555123@gmail.com>
2024-11-16 21:22:15 +0500

    array inserting 50% Practice Completed

--- Array Inserting/script.js
+++ Array Inserting/script.js
@@ -1,1 +1,7 @@
-console.log("Array Inserting");
+const data = [34, 6, 12, 0, 89];
+let newElement = 70;
+let position = 2;
+
+for (let i = data.length - 1; i >= 0; i--) {
+    console.log(data[i]);
+}
